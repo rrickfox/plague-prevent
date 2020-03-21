060abe4a691a8130a891718a3b3798f5c9750b92
JP <jeanpasqual2@gmail.com>
2020-03-21 15:35:26 +0100

    tmpro and menu

---
+++ Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject diseaseMenu;
+    public GameObject countryMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainMenu.SetActive(true);
+        diseaseMenu.SetActive(false);
+        countryMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ToMainMenu()
+    {
+        mainMenu.SetActive(true);
+        diseaseMenu.SetActive(false);
+        countryMenu.SetActive(false);
+    }
+
+    public void ToDiseaseMenu()
+    {
+        mainMenu.SetActive(false);
+        diseaseMenu.SetActive(true);
+        countryMenu.SetActive(false);
+    }
+
+    public void ToCountryMenu()
+    {
+        mainMenu.SetActive(false);
+        diseaseMenu.SetActive(false);
+        countryMenu.SetActive(true);
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
