fbb8e20e6524acaef33f51eb8b0fa68cabcf9856
Progyo <nadim@darava.com>
2020-03-21 11:40:30 +0100

    Added CameraMovement.cs with zoom and pan functionality

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float sensitivity = 1.5f;            //Movement sensitivity
+
+    public bool panPressed = false;             //Is pan button pressed
+
+    public float maxZoom = 2.5f;                //Maximum zoom level
+    public float minZoom = 7f;                  //Minimum zoom level
+
+    private void Update()
+    {
+        panPressed = Input.GetMouseButton(2);
+
+        if (panPressed)
+        {
+            float xMov = Input.GetAxis("Mouse X") * Time.deltaTime * sensitivity;
+            float yMove = Input.GetAxis("Mouse Y") * Time.deltaTime * sensitivity;
+            Camera.main.transform.position -= new Vector3(xMov, yMove);
+        }
+        //Debug.Log(Mathf.Abs(Input.GetAxis("Mouse ScrollWheel")));
+        if(Mathf.Abs(Input.GetAxis("Mouse ScrollWheel")) > 0f)
+        {
+            //Clamps zoom level
+            Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - Input.GetAxis("Mouse ScrollWheel"), maxZoom, minZoom);
+        }
+    }
+}
