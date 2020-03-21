997f40b779da3de54108d3410878935626c8d2dc
Progyo <nadim@darava.com>
2020-03-21 12:09:05 +0100

    Added Law constructor and example for a law in Country. Also reorganized comments

--- Assets/Scripts/Abstract/Country.cs
+++ Assets/Scripts/Abstract/Country.cs
@@ -6,21 +6,28 @@ using System.Linq;
 public abstract class Country : MonoBehaviour
 {
 
-    public string countryName;          //Name of the country
 
-    public bool democratic;             //Whether the country is democratic or not
-    public int icuBeds => states.Sum(s => s.icuBeds);              //How many total ICU Beds there are
-    public int startingBudget;          //The starting budget for a country
-    public int currentBudget;           //The current budget for a country
+    public string countryName;                                      //Name of the country
 
-    public int infected => states.Sum(s => s.infected);                //How many total people are currently infected
+    public bool democratic;                                         //Whether the country is democratic or not
+    public List<Law> laws = new List<Law>()
+    {
+        new Law("Washing Hands", 10, 0.1f)
+    };
+
+
+    public int icuBeds => states.Sum(s => s.icuBeds);               //How many total ICU Beds there are
+    public int startingBudget;                                      //The starting budget for a country
+    public int currentBudget;                                       //The current budget for a country
+
+    public int infected => states.Sum(s => s.infected);             //How many total people are currently infected
     public int cured => states.Sum(s => s.cured);                   //How many total people have been cured
-    public int dead => states.Sum(s => s.dead);                    //How many total people have died
-    public int population => states.Sum(s => s.population);              //Total population of the country
+    public int dead => states.Sum(s => s.dead);                     //How many total people have died
+    public int population => states.Sum(s => s.population);         //Total population of the country
 
-    public List<State> states;          //List of states in the country
+    public List<State> states;                                      //List of states in the country
 
-    public List<Country> neighbours;    //List of neighbouring countries
+    public List<Country> neighbours;                                //List of neighbouring countries
 
 
     //Enforce a specific law (Depending on implementation democratic or totalitarian)
--- Assets/Scripts/Law.cs
+++ Assets/Scripts/Law.cs
@@ -7,4 +7,11 @@ public struct Law
     public string name;             //Name of the law
     public int satisfaction;        //How happy the civilians are with this law
     public float dampener;          //Dampening effect of this law on the infection rate of the disease
+
+    public Law(string name, int satisfaction, float dampener)
+    {
+        this.name = name;
+        this.satisfaction = satisfaction;
+        this.dampener = dampener;
+    }
 }
