e764d76cc68b31e568189acc524f44fad3a2cbe3
Progyo <nadim@darava.com>
2020-03-22 01:27:46 +0100

    Fixed merge

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -2,10 +2,6 @@ using UnityEngine;
 
 public static class Constants
 {
-<<<<<<< HEAD
     public static readonly float timeSpeed = 30f;
     public static readonly float timeScale = 50f * 60f * timeSpeed;
-=======
-    public static readonly float timeScale = 24f * 60f * 2f; //days to ticks
->>>>>>> 93a3f6dd9b0bee30468cabbc63787bf9a8c63784
 }
